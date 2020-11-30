9434af78881951a4a963af85aa39ef3f8233747a
adam allalou <aabou9@gmail.com>
2020-11-30 16:58:54 +0100

    add header and footer

--- functions.php
+++ functions.php
@@ -1,6 +1,8 @@
 <?php
 function add_files() {
   wp_enqueue_style('university_styles', get_stylesheet_uri());
+  wp_enqueue_style('university_font', "https://fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i");
+  wp_enqueue_style('university_icons', "https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css");
 }
 
 add_action('wp_enqueue_scripts', 'add_files');
