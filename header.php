0bdef809c208f44f7437d1a69a857c2fb973b4fb
adam allalou <aabou9@gmail.com>
2020-12-07 13:15:41 +0100

    add responsive tags

--- header.php
+++ header.php
@@ -1,9 +1,11 @@
 <!DOCTYPE html>
-<html lang="en">
+<html <?php language_attributes(); ?>>
 <head>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta charset="<?php bloginfo('charset') ?>">
   <?php wp_head()  ?>
 </head>
-<body>
+<body <?php body_class(); ?>>
   <header class="site-header">
     <div class="container">
       <h1 class="school-logo-text float-left">
