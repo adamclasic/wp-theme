50fa846f4ece5f8c59093b843f7378648090dd1a
adam allalou <aabou9@gmail.com>
2020-12-10 11:55:23 +0100

    add event archive

---
+++ archive-event.php
@@ -0,0 +1,31 @@
+<?php get_header(); ?>
+
+<div class="page-banner">
+      <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('images/library-hero.jpg') ?>);"></div>
+      <div class="page-banner__content container t-center c-white">
+        <h1 class="headline headline--large">Welcome!</h1>
+        <h2 class="headline headline--medium">We think you&rsquo;ll like it here.</h2>
+        <h3 class="headline headline--small">Why don&rsquo;t you check out the <strong>major</strong> you&rsquo;re interested in?</h3>
+        <a href="#" class="btn btn--large btn--blue">Find Your Major</a>
+      </div>
+    </div>
+    <div class="container container--narrow page-section">
+    <?php
+          
+          while(have_posts()) {
+            the_post();
+            ?>
+          <div class="event-summary">
+            <a class="event-summary__date t-center" href="<?php the_permalink(); ?>">
+              <span class="event-summary__month">Mar</span>
+              <span class="event-summary__day">25</span>
+            </a>
+            <div class="event-summary__content">
+              <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
+              <p><?php echo get_the_excerpt(); ?><a href="<?php the_permalink(); ?>" class="nu gray">Learn more</a></p>
+            </div>
+          </div>
+          <?php } ?>
+    </div>
+
+<?php get_footer(); ?>
---
+++ single-event.php
@@ -0,0 +1,31 @@
+<?php get_header();
+while(have_posts()) {
+  the_post();?>
+  
+<div class="page-banner">
+    <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('images/ocean.jpg'); ?>);"></div>
+    <div class="page-banner__content container container--narrow">
+      <h1 class="page-banner__title"><?php the_title(); ?></h1>
+      <div class="page-banner__intro">
+        <p>DONT FORGET TO CHNAGE THIS.</p>
+      </div>
+    </div>  
+  </div>
+
+  <div class="container container--narrow page-section">
+
+    <div class="metabox metabox--position-up metabox--with-home-link">
+      <p><a class="metabox__blog-home-link" href="<?php echo site_url('/blog') ?>"><i class="fa fa-home" aria-hidden="true"></i> Back to Blog Homg</a> <span class="metabox__main">Posted by <?php echo get_the_author(); ?> on <?php the_time(); ?> in <?php echo get_the_category_list(', '); ?></span></p>
+    </div>
+
+<div class="generic-content">
+  <h1><?php the_title();?></h1>
+
+  <?php the_content(); ?>
+  
+  <?php
+    }
+  ?>
+</div>
+  </div>
+<?php get_footer(); ?>
