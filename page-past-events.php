b51cd0264a0872c88b12f4038e05b148babd0bcc
adam allalou <aabou9@gmail.com>
2020-12-10 23:33:08 +0100

    add past-events page

---
+++ page-past-events.php
@@ -0,0 +1,52 @@
+<?php get_header(); ?>
+
+<div class="page-banner">
+      <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('images/library-hero.jpg') ?>);"></div>
+      <div class="page-banner__content container t-center c-white">
+        <h1 class="headline headline--large">Welcome!</h1>
+        <h2 class="headline headline--medium">We think you&rsquo;ll like it here.</h2>
+        <h3 class="headline headline--small">Why don&rsquo;t you check out the <strong>major</strong> you&rsquo;re interested in?</h3>
+        <a href="#" class="btn btn--large btn--blue">Find Your Major</a>
+      </div>
+    </div>
+    <div class="container container--narrow page-section">
+    <?php
+          $pastEventsQuery = new WP_Query(array(
+            'paged' => get_query_var('paged', 1),
+            'post_type' => 'event',
+            'meta_key' => 'event_date',
+            'order_by' => 'meta_value_num',
+            'order' => 'ASC',
+            'meta_query' => array(
+              array(
+                'key' => 'event_date',
+                'compare' => '<',
+                'value' => Date('Ymd')
+              ),
+            ),
+          ));
+          while($pastEventsQuery->have_posts()) {
+            $pastEventsQuery->the_post();
+            ?>
+          <div class="event-summary">
+            <a style="background-color: #aaa;" class="event-summary__date t-center" href="<?php the_permalink(); ?>">
+              <span class="event-summary__month"><?php 
+              $monthDate = new DateTime(get_field('event_date')); 
+              echo $monthDate->format('M');
+              ?></span>
+              <span class="event-summary__day"><?php echo $monthDate->format('d'); ?></span>
+            </a>
+            <div class="event-summary__content">
+              <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
+              <p><?php echo wp_trim_words(get_the_excerpt(), 10); ?><a href="<?php the_permalink(); ?>" class="nu gray">Learn more</a></p>
+            </div>
+          </div>
+          <?php
+          echo paginate_links(array(
+            'total' => $pastEventsQuery->max_num_pages,
+          ));
+          }
+          ?>
+    </div>
+
+<?php get_footer(); ?>
