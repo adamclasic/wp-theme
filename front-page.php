b620029af8c5e11e62e45612d62db473a30bcbe0
adam allalou <aabou9@gmail.com>
2020-12-10 17:06:00 +0100

    add custom field of event-date

--- front-page.php
+++ front-page.php
@@ -24,8 +24,11 @@
             ?>
           <div class="event-summary">
             <a class="event-summary__date t-center" href="<?php the_permalink(); ?>">
-              <span class="event-summary__month">Mar</span>
-              <span class="event-summary__day">25</span>
+              <span class="event-summary__month"><?php 
+              $monthDate = new DateTime(get_field('event_date')); 
+              echo $monthDate->format('M');
+              ?></span>
+              <span class="event-summary__day"><?php echo $monthDate->format('d'); ?></span>
             </a>
             <div class="event-summary__content">
               <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
--- single-event.php
+++ single-event.php
@@ -15,7 +15,7 @@ while(have_posts()) {
   <div class="container container--narrow page-section">
 
     <div class="metabox metabox--position-up metabox--with-home-link">
-      <p><a class="metabox__blog-home-link" href="<?php echo site_url('/blog') ?>"><i class="fa fa-home" aria-hidden="true"></i> Back to Blog Homg</a> <span class="metabox__main">Posted by <?php echo get_the_author(); ?> on <?php the_time(); ?> in <?php echo get_the_category_list(', '); ?></span></p>
+      <p><a class="metabox__blog-home-link" href="<?php echo site_url('/event') ?>"><i class="fa fa-home" aria-hidden="true"></i> Back to all Events</a> <span class="metabox__main">Posted by <?php echo get_the_author(); ?> on <?php the_time(); ?> in <?php echo get_the_category_list(', '); ?></span></p>
     </div>
 
 <div class="generic-content">
