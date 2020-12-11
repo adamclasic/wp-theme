ff5b7011a09ef2777383874ac63d9e8508ef37a7
adam allalou <aabou9@gmail.com>
2020-12-11 18:56:41 +0100

    update archive-programe url query

--- functions.php
+++ functions.php
@@ -55,6 +55,23 @@ add_action('init', 'add_custom_posts');
 
 function customDefaultEventsQuery($query)
 {
+  //custom archive-program URL query
+  if (!is_admin() AND is_post_type_archive('program') AND $query->is_main_query()) {
+    // $query->set('meta_key', 'event_date');
+    $query->set('posts_per_page', -1);
+    $query->set('order_by', 'title');
+    $query->set('order', 'DESC');
+    // $query->set('meta_query', array(
+    //     array(
+    //       'key' => 'event_date',
+    //       'compare' => '>=',
+    //       'value' => Date('Ymd')
+    //     )
+    //   )
+    // );
+  }
+
+  //custom archive-event URL query
   if (!is_admin() AND is_post_type_archive('event') AND $query->is_main_query()) {
     $query->set('meta_key', 'event_date');
     $query->set('order_by', 'meta_value_num');
