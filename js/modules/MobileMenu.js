174e7ef33a39c3617037859f941818a9b91d3bd8
adam allalou <aabou9@gmail.com>
2020-12-15 11:28:38 +0100

    add search modul

--- js/modules/MobileMenu.js
+++ js/modules/MobileMenu.js
@@ -1,19 +1,20 @@
+import $ from 'jquery';
+
 class MobileMenu {
   constructor() {
-    this.menu = document.querySelector(".site-header__menu")
-    this.openButton = document.querySelector(".site-header__menu-trigger")
-    this.events()
+    this.menu = $(".site-header__menu");
+    this.openButton = $(".site-header__menu-trigger");
+    this.events();
   }
 
   events() {
-    this.openButton.addEventListener("click", () => this.openMenu())
+    this.openButton.on("click", this.openMenu.bind(this));
   }
 
   openMenu() {
-    this.openButton.classList.toggle("fa-bars")
-    this.openButton.classList.toggle("fa-window-close")
-    this.menu.classList.toggle("site-header__menu--active")
+    this.openButton.toggleClass("fa-bars fa-window-close");
+    this.menu.toggleClass("site-header__menu--active");
   }
 }
 
-export default MobileMenu
+export default MobileMenu;
